2668398b88c32be94e87ee3dd7671831891da88f
Amir Ghanem <amir.ghanem@eshaam.com>
2024-02-12 22:47:19 +0200

    --

---
+++ DbContexts/Migrations/20240212195052_updateCouponUsage.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BiyLineApi.DbContexts.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateCouponUsage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProductName",
+                table: "CouponsUsages");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ItemCount",
+                table: "CouponsUsages",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ItemCount",
+                table: "CouponsUsages");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProductName",
+                table: "CouponsUsages",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}

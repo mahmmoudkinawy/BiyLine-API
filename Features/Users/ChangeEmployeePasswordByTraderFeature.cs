7013685ee3c06a8be5c99f78477c08cbef3a5c85
amrhweedy <amrhweedy@gmail.com>
2024-02-16 11:37:42 +0200

    .

---
+++ Features/Users/ChangeEmployeePasswordByTraderFeature.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Identity.Client;
+
+namespace BiyLineApi.Features.Users;
+
+public sealed class ChangeEmployeePasswordByTraderFeature
+{
+    public sealed class Request : IRequest<Result<Response>>
+    {
+        public string NewPassword { get; set; }
+    }
+    
+    public sealed class Response
+    {
+        public string Message { get; set; }
+    }
+}
